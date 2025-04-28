b4a17e15ecd2ea34d9d33e991a71173c92026c81
Rocky-Sujit <en.sujitkumar@gmail.com>
2025-04-28 21:37:41 +0530

    Add files via upload

---
+++ assignment_2.py
@@ -0,0 +1,15 @@
+### Task 1: Check if a Number is Even or Odd
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(num, "Number is even")
+else:
+    print(num, "Number is odd")
+
+num1 = int(input("Enter a number: "))
+
+if num1 % 2 == 0:
+    print(num1, "Number is even")
+else:
+    print(num1, "Number is odd")
