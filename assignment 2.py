e1198275d1f1a0f078baa99a9b611160708c6f86
Rocky-Sujit <en.sujitkumar@gmail.com>
2025-04-28 17:47:10 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,22 @@
+## Task 1: Perform Basic Mathematical Operations
+'''
+a = 5
+b = 10
+
+add = a + b
+sub = a-b
+div = a/b
+mul = a*b
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
+'''
+## Task 2: Create a Personalized Greeting
+
+first_name = input("Enter First Name: ")
+last_name = input("Enter Last Name: ")
+
+print("Hello, ",first_name , last_name,"Welcome to the Python Program" )
+
+'''
