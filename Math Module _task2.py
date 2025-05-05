5210d4b29a0279a4f24adc31d35a9741e0031c3b
Rocky-Sujit <en.sujitkumar@gmail.com>
2025-05-06 01:55:56 +0530

    Add files via upload

---
+++ Math Module _task2.py
@@ -0,0 +1,11 @@
+### Task 2: Using the Math Module for Calculations
+
+
+n = float(input('Enter a number:'))
+
+from math import sqrt
+from math import log
+from math import radians
+print('Square root: ', sqrt(n))
+print('logarithm: ',log(n))
+print('Sine: ',radians(n))
